f7d0423e2a25bf035a7a4aa26d1409d8e3e81bf0
Animesh Jha <jha.animesh01@gmail.com>
2023-02-25 19:16:19 +0530

    start

--- ServiceProviders/ManagerModel.py
+++ ServiceProviders/ManagerModel.py
@@ -1,6 +1,6 @@
-# Table : Messages (used for finding which broker has a certain message with an offset)
-# [topic_name, msg_id(increasing int), broker_id, partition_id]
-# msg_id is a global counter for all messages
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 # Table : Brokers (maps the ip/port of each broker)
 # [broker_id, endpoint, last_beat_timestamp]
@@ -12,7 +12,31 @@
 # used in round_robin(or random) selection
 # [topic_name, partition_id, broker_id]
 
-# Table : Offsets(self explanatory)
+
+# Table : Messages (used for finding which broker has a certain message with an offset)
+# [topic_name, id(increasing int), broker_id, partition_id]
+# id is a global counter for all messages
+
+class ManagerMessageView(db.Model):
+    __tablename__ = 'ManagerMessageView'
+    topic_name = db.Column(db.String(),)
+    partition_id = db.Coumn(db.String(),db.ForeignKey('PartitionMetadata.partition_id'))
+    broker_id = db.Coumn(db.String(),db.ForeignKey('BrokerMetadata.broker_id'))
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self,topic_name,partition_id,broker_id):
+        self.broker_id=broker_id
+        self.topic_name=topic_name
+        self.partition_id=partition_id
+
+    @staticmethod
+    def getBrokerID(targetTopic, targetPartitionId,targetOffset):
+        return ManagerMessageView.query.filter_by(topic_name=targetTopic, partition_id = targetPartitionId)[targetOffset].broker_id
+    
+
+
+
+# Table : Offsets(self explanatory)  
 # [Consumer_id, topic_name, partition_id(null if subscribed to entire topic), offset]
 
 # Table : Producers
