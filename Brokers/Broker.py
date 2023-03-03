6b7428e4c83368348268fbcca2355e72cffdc989
Parth Jindal <pmjindal@gmail.com>
2023-03-03 23:49:28 +0530

    Implement broker
    Co-authored-by: Abhinandan De <abhinandan0316@gmail.com>

--- Brokers/Broker.py
+++ Brokers/Broker.py
@@ -4,65 +4,68 @@ import threading
 import uuid
 from typing import Dict, List, Tuple, Set
 from concurrent.futures import ThreadPoolExecutor
+from BrokerModels import db, TopicName, TopicMessage
 
-# TODO: define enum for success and failure codes   
+# TODO: define enum for success and failure codes
+
 
 class LoggingQueue:
     def __init__(self):
-        self.DetailsDB, self.NameDB, self.MessageDB = BrokerModels.return_objects()
+        pass
 
     def create_topic(self, topic_name: str, partition_id) -> None:
-        if not self.NameDB.CheckTopic(topic_name=topic_name, partition_id=partition_id):
-            self.NameDB.CreateTopic(topic_name=topic_name, partition_id=partition_id)
+        if not TopicName.CheckTopic(topic_name=topic_name, partition_id=partition_id):
+            TopicName.CreateTopic(topic_name=topic_name,
+                                  partition_id=partition_id)
             print(f"Topic {topic_name} with partition {partition_id} created.")
-            return 1 
+            return 1
         else:
             print(f"Topic {topic_name} with partition {partition_id} already exists.")
             return -1
 
-    def list_topics(self) -> List[str]:
-        topic_list = self.NameDB.ListTopics()
-        print(topic_list)
-        # import ipdb; ipdb.set_trace()
-        return list(topic_list)
+    def list_topics(self) -> List[Tuple[str, str]]:
+        topic_part_list = TopicName.ListTopics()
+        return topic_part_list
 
-    def enqueue(self, message_id:int, topic_name:str, partition_id:str, producer_id: int, message: str) -> int:
-        
-        producer_id = str(producer_id)
-        if not self.NameDB.CheckTopic(topic_name=topic_name, partition_id=partition_id):
+    def enqueue(self, message: str, topic: str, partition_id: int) -> int:
+        # check if (topic, partition_id) exists else create it
+        if not TopicName.CheckTopic(topic_name=topic, partition_id=partition_id):
+            TopicName.CreateTopic(topic_name=topic, partition_id=partition_id)
+            print(f"Topic {topic} with partition {partition_id} created.")
+
+        status = TopicMessage.addMessage(
+            topic_name=topic, partition_id=partition_id, message=message)
+        if status == 1:
+            print(f"Message '{message}' added to topic {topic} with partition {partition_id}.")
+            return 1
+        else:
+            print(f"Message '{message}' could not be added to topic {topic} with partition {partition_id}.")
+            return -1
+
+    def dequeue(self, topic_name: str, partition_id: int, offset: int, *args, **kwargs) -> str:
+
+        if not TopicName.CheckTopic(topic_name=topic_name, partition_id=partition_id):
             print(f"Topic {topic_name} with partition {partition_id} does not exist.")
             return -1
-        
-        # self.topics[topic_name].add_log(message,message_metadata=f"{producer_id}")
-        self.MessageDB.addMessage(message_id=message_id, topic_name=topic_name, partition_id=partition_id, producer_id=producer_id, message=message)
-        print(f"Producer {producer_id} enqueued message '{message}' to topic {topic_name} with partition {partition_id}.")
-        return 1
 
-    def dequeue(self, message_id: int, topic_name: str, partition_id: str) -> str:
-
-        if not self.NameDB.CheckTopic(topic_name=topic_name, partition_id=partition_id):
-            print(f"Topic {topic_name} with parititon {partition_id} does not exist.")
-            return -1
-        
-        message = self.MessageDB.retrieveMessage(message_id=message_id, topic_name=topic_name, partition_id=partition_id)
-
-        if message == -1:
-            print(f"No message in queue!!!")
-            return -2
-        else:
+        message = TopicMessage.retrieveMessage(topic_name=topic_name, partition_id=partition_id, 
+                                               offset=offset)
+        if (isinstance(message, str)):
             print(f"Message '{message}' from topic {topic_name}, partition {partition_id}.")
             return message
+        elif message == -1:
+            print(f"No message in queue!!!")
+            return -2
 
-    def size(self, topic_name: str, partition_id: str) -> int:
-        if not self.NameDB.CheckTopic(topic_name=topic_name, partition_id=partition_id):
+    def size(self, topic_name: str, partition_id: str, offset) -> int:
+        if not TopicName.CheckTopic(topic_name=topic_name, partition_id=partition_id):
             print(f"Topic {topic_name} with partition {partition_id} does not exist.")
             return -1
-            
-        return self.MessageDB.getSizeforTopic(topic_name=topic_name, partition_id=partition_id)
-    
+        return TopicMessage.getSizeforTopic(topic_name=topic_name, partition_id=partition_id, offset=offset)
+
     def set_details(self, broker_id, ip, port):
         self.DetailsDB.addBrokerDetails(broker_id=broker_id, ip=ip, port=port)
-    
+
     def get_details(self):
         broker = self.DetailsDB.getBrokerDetails()
         if broker == -1:
--- Brokers/BrokerWrapper.py
+++ Brokers/BrokerWrapper.py
@@ -1,16 +1,13 @@
-# TODO: Implement Flask Interface \
 from flask import Flask, request
 from Broker import LoggingQueue
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from BrokerModels import db, TopicName
+from BrokerModels import db
 
-import json
 import uuid
 import argparse
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgrespassword@127.0.0.1:5432/flasksql"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@127.0.0.1:5430/postgres"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db.init_app(app)
@@ -24,66 +21,34 @@ broker = LoggingQueue()
 def hello_world():
 	return "<h1> Hello WOrld wow</h1>"
 
-@app.route("/topics", methods=["POST", "GET"])
-def topics():
-	print(request.method)
-	# print(broker.__dict__)
-	# return "hi"
-	if request.method == "POST":
-		dict = request.get_json()
-		topic_name = dict['topic_name']
-		partition_id = dict['partition_id']
-		response = {}
-		status = broker.create_topic(topic_name, partition_id)
-		if status == 1:
-			response["status"] = "Success"
-			response["message"] = f"Topic {topic_name} with partition {partition_id} created successfully!"
-		else:
-			response["status"] = "Failure"
-			response["message"] = f"Topic {topic_name} with partition {partition_id} already exists!"
-		
-		return response
-		# print(dict['topic_name'])
-		# TODO : Interact with logging queue and return valid response 
-		
-		# return "test", 205
-	
-	else:
-		# TODO : Return topic list
-		return {"topics" : broker.list_topics()}
+# todo: add topic name and partition id endpoint
 
 @app.route("/producer/produce", methods=["POST"])
-def enque():
+def enqueue():
 	dict = request.get_json()
 	topic = (dict['topic'])
-	producer_id = uuid.UUID(dict['producer_id'])
+	partition_id = (dict['partition_id'])
 	message = dict['message']
 
-	
-	status = broker.enqueue(topic_name=topic, producer_id=producer_id, message=message)
+	status = broker.enqueue(message=message, topic=topic, partition_id=partition_id)
 	response = {}
 
 	if status == 1:
 		response["status"] = "Success"
 	else:
 		response["status"] = "Failure"
-		if status == -1:
-			response["message"] = f"Topic {topic} does not exist."
-		elif status == -2:
-			response["message"] = f"Producer {producer_id} is not registered for topic {topic}."
 
 	return response
-	# else return error
-	# return {"status"  : "failure",
-	# 		  "message" : "topic not found"}
 
 @app.route("/consumer/consume", methods=["GET"])
 def dequeue():
 	dict = request.get_json()
 	topic = (dict['topic'])
 	consumer_id = uuid.UUID(dict['consumer_id'])
+	partition_id = (dict['partition_id'])
+	offset = (dict['offset'])
 	# if topic exists send consumer id
-	status = broker.dequeue(topic_name=topic, consumer_id=consumer_id)
+	status = broker.dequeue(topic_name= topic, partition_id= partition_id, offset= offset)
 	response = {}
 
 	if isinstance(status, str) :
@@ -99,17 +64,15 @@ def dequeue():
 			response["message"] = f"No more messages for {consumer_id}"
 
 	return response
-	# else return error
-	# return {"status"  : "failure",
-	# 		  "message" : "topic not found"}
 
 @app.route("/size", methods=["GET"])
 def size():
 	dict = request.get_json()
 	topic = (dict['topic'])
-	consumer_id = uuid.UUID(dict['consumer_id'])
+	partition_id = (dict['partition_id'])
+	offset = (dict['offset'])
 
-	status = broker.size(topic_name= topic, consumer_id= consumer_id)
+	status = broker.size(topic_name= topic,partition_id=partition_id, offset= offset)
 	response = {}
 
 	if status >=0 :
@@ -120,26 +83,14 @@ def size():
 		if status == -1:
 			response["message"] = f"Topic {topic} does not exist."
 		elif status == -2:
-			response["message"] = f"Consumer {consumer_id} is not registered for topic {topic}."
+			response["message"] = f"Consumer is not registered for topic {topic}."
 
 	return response
-
-
-def register_broker(manager_ip, manager_port) -> str:
-	# 1. Check if broker exists
-	# 2. If not, register broker
-	pass
 
 def cmdline_args():
 	# create parser
 	parser = argparse.ArgumentParser()
-
-	parser.add_argument("-i", "--ip", help="ip address", type=str, default="127.0.0.1")
 	parser.add_argument("-p", "--port", help="port number", type=int, default=8080)
-
-	parser.add_argument("-mi", "--manager_ip", help="manager ip address", type=str)
-	parser.add_argument("-mp", "--manager_port", help="manager port number", type=int)
-
 	return parser.parse_args()
 
 if __name__ == '__main__':
@@ -150,6 +101,4 @@ if __name__ == '__main__':
 		db.create_all() # <--- create db object.
 	
 	app.run(debug=True, port = args.port)
-	register_broker(args.manager_ip, args.manager_port)
-
 	# TODO: create a thread that periodically sends heartbeat to manager
