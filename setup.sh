867d8c237b4285a04df48e94c998c32f0be15e7e
Animesh Jha <jha.animesh01@gmail.com>
2023-03-06 18:35:26 +0530

    meh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
-docker build . -t mbq:latest
-docker compose up
+sudo docker build . -t mbq:latest
+mkdir -p data/reverse_proxy
+mkdir -p data/write_manager
+mkdir -p data/read_manager_one
+mkdir -p data/read_manager_two
+mkdir -p data/broker_one
+mkdir -p data/broker_two
+mkdir -p data/broker_three
+mkdir -p data/main_db_one
+mkdir -p data/main_db_two
+mkdir -p data/db_one
+mkdir -p data/db_two
+mkdir -p data/db_three
+sudo docker compose up
