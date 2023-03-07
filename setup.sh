66df714a614ed99e1d6eab3cb1e527e734ccac1f
Animesh Jha <jha.animesh01@gmail.com>
2023-03-07 21:58:05 +0530

    working on mine :)

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+sudo docker compose down
+sudo rm -rf ../data/
 sudo docker build . -t mbq:latest
 mkdir -p ../data/reverse_proxy
 mkdir -p ../data/write_manager
@@ -12,4 +14,20 @@ mkdir -p ../data/main_db_three
 mkdir -p ../data/db_one
 mkdir -p ../data/db_two
 mkdir -p ../data/db_three
+sudo docker compose up -d  main_db_three
+echo "Starting postgres_master node..."
+sleep 20  # Waits for master note start complete
+docker exec -it main_db_three sh /etc/postgresql/init-script/init-db.sh
+echo "Restart master node"
+sudo docker compose restart main_db_three 
+sleep 5
+
+echo "Starting read manager  nodes..."
+sudo docker compose up -d  main_db_one
+sudo docker compose up -d  main_db_two
+sleep 20  # Waits for note start complete
+
+echo "Done"
+
+
 sudo docker compose up
