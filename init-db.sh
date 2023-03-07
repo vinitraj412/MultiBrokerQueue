e1fa33a69316b3cc708980fffb9c659af520cce0
Animesh Jha <jha.animesh01@gmail.com>
2023-03-07 18:17:10 +0530

    beautiful

--- init-db.sh
+++ init-db.sh
@@ -2,7 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username postgres --dbname "$hostname" <<-EOSQL
-	CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password'
+	CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password';
     SELECT * FROM pg_create_physical_replication_slot('replication_slot_read_1');
     SELECT * FROM pg_create_physical_replication_slot('replication_slot_read_2');
 EOSQL
+pg_basebackup -D /tmp/postgres_read_1 -S replication_slot_read_1 -X stream -P -U replicator -Fp -R
+pg_basebackup -D /tmp/postgres_read_2 -S replication_slot_read_2 -X stream -P -U replicator -Fp -R
